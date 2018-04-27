acf78eafca9cf9e2aeabe0a0dc702429baec0aa1
yuna iwamura <iwamura.yuna@moneyforward.co.jp>
2018-04-27 18:29:41 +0900

    Add password reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,8 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user,   only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
+
   def new
   end
 
@@ -15,6 +19,45 @@ class PasswordResetsController < ApplicationController
     end
   end
 
+  def update
+    if params[:user][:password].empty?                  # (3) への対応
+      @user.errors.add(:password, :blank)
+      render 'edit'
+    elsif @user.update_attributes(user_params)          # (4) への対応
+      log_in @user
+      @user.update_attribute(:reset_digest, nil)
+      flash[:success] = "Password has been reset."
+      redirect_to @user
+    else
+      render 'edit'                                     # (2) への対応
+    end
+  end
+
   def edit
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+
+  def get_user
+    @user = User.find_by(email: params[:email])
+  end
+
+   # 正しいユーザーかどうか確認する
+  def valid_user
+    unless (@user && @user.activated? && @user.authenticated?(:reset, params[:id]))
+      redirect_to root_url
+    end
+  end
+
+  # 期限切れかどうかを確認する
+  def check_expiration
+    if @user.password_reset_expired?
+      flash[:danger] = "Password reset has expired."
+      redirect_to new_password_reset_url
+    end
   end
 end
